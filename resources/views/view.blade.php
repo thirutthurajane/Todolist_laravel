ee561ad03825db151bf3fbd76c86b3ea583771d7
thirutthurajane <thirut.thurajane@gmail.com>
2022-02-15 14:00:25 +0700

    Delete Duplicate Code

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -11,7 +11,6 @@
         <div class="card-content">
             {{ Form::model($todo,array('url' => ['edit',$todo->id], 'method' => 'PUT', 'class'=>'col-md-12')) }}
                 {{ csrf_field() }}
-                {{ method_field('PUT')  }}
                 <div class="field">
                     {{ Form::label('Title','Title',array('class'=>'label'))  }}
                     <div class="control">
